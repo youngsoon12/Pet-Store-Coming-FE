ca08726b16edd59490896b26559237a08615266f
JGW-Korea <jgw6372@gmail.com>
2024-10-24 01:20:14 +0900

    feat: Global Layout Component 생성

---
+++ src/components/global/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+import { Flex as Wrapper } from './Flex';
+
+function Layout({ children }) {
+  return (
+    <Wrapper direction="column" align="center">
+      {/* Header 컴포넌트 import */}
+      {children}
+      {/* Footer 컴포넌트 import */}
+    </Wrapper>
+  );
+}
+
+export default Layout;
--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -4,14 +4,17 @@ import { Global } from '@emotion/react';
 // 페이지 컴포넌트 import
 import Home from '@pages/Home';
 import { globalStyle } from '@styles/global';
+import Layout from '../components/global/Layout';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Global styles={globalStyle} />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
+      <Layout>
+        <Global styles={globalStyle} />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
