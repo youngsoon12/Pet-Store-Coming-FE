d1704174c977ca28ba8ab7074993f4f26226d7ba
JGW-Korea <jgw6372@gmail.com>
2024-10-24 00:13:43 +0900

    feat: Vite 절대 경로 별칭(aliaas) 설정 (#4)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: 'public',
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@assets', replacement: '/src/assets' },
+    ],
+  },
+});
