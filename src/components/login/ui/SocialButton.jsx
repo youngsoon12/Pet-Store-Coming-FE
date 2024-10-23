0483fef908517d7c68a60ea96467ba3430a4f457
JGW-Korea <jgw6372@gmail.com>
2024-10-24 02:59:19 +0900

    feat: 소셜 로그인 버튼 생성 완료 (#3)

--- src/components/login/ui/SocialButton.jsx
+++ src/components/login/ui/SocialButton.jsx
@@ -1,15 +1,46 @@
+import styled from '@emotion/styled';
 import React from 'react';
+import { media } from '../../../styles/media';
 
 /**
  * @description: 로그인 페이지에 사용되는 소셜 로그인 버튼 컴포넌트를 반환
  * @param { Object } props - 소셜 로그인 버튼 컴포넌트에 전달되는 속성
  * @param { string } props.bgColor - 배경 색상
- * @param { string } props.imageUrl - 소셜 로그인에서 제공하는 이미지
+ * @param { string } props.img - 소셜 로그인에서 제공하는 이미지
  * @param { function } props.onClick - 나중에 주석 수정
  * @returns
  */
-function Button({ bgColor, imageUrl }) {
-  return <div>Button</div>;
+export function SocialButtonComponent({ bgColor, img }) {
+  return <SocialButton bgColor={bgColor} img={img} />;
 }
 
-export default Button;
+const SocialButton = styled.div`
+  width: 100%;
+  height: 45px;
+  background-color: ${({ bgColor }) => bgColor};
+  cursor: pointer;
+  overflow: hidden;
+  border-radius: 7px;
+
+  background-image: ${({ img }) => `url(${img})`};
+  background-repeat: no-repeat;
+  background-position: center;
+
+  max-width: 400px;
+  min-width: 183px;
+
+  // 1. 데스크탑 화면 Media Query 정의
+  ${media.desktop`
+    width: 70%;
+  `}
+
+  // 2. 태블릿 화면 Media Query 정의
+  ${media.tablet`
+    width: 60%;
+  `}
+
+  // 3. 모바일 화면에 맞는 크기로 큰 틀 정의
+  ${media.mobile`
+    width: 50%;
+  `}
+`;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,26 @@
 import React, { useMemo } from 'react';
 import { LoginAPI } from '@apis/LoginPage/LoginAPI.js';
+import { SocialButtonComponent as SocialButton } from '@components/login/ui/SocialButton.jsx';
+
+// Login Page에 필요한 Component import
+// import { SocialButtonComponent as SocialButton } from
 
 function LoginPage() {
   const apiClass = useMemo(() => new LoginAPI(), []);
 
-  return <></>;
+  return (
+    <>
+      <SocialButton
+        bgColor={'#FEE500'}
+        img={
+          new URL(
+            '@assets/images/login/kakao_login_medium_narrow.png',
+            import.meta.url
+          ).href
+        }
+      />
+    </>
+  );
 }
 
 export default LoginPage;
