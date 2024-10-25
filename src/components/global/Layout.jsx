7e89c1d4582045cf5ad3707b16136260a38b9fa7
JGW-Korea <jgw6372@gmail.com>
2024-10-25 14:17:38 +0900

    feat: Header 컴포넌트 Import

--- src/components/global/Layout.jsx
+++ src/components/global/Layout.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 import { Flex as Wrapper } from './Flex';
+import Header from '@components/global/Header';
 
 function Layout({ children }) {
   return (
     <Wrapper direction="column" align="center">
-      {/* Header 컴포넌트 import */}
+      <Header />
+
       {children}
       {/* Footer 컴포넌트 import */}
     </Wrapper>
