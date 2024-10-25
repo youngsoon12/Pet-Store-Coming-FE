4e3b78dceb3ff5e8634a1fcd1a4cb99f7e29ad83
khj <ghghjj652@naver.com>
2024-10-25 15:40:02 +0900

    feat: Footer Container, Footer Left Panel CSS 완성

--- src/components/global/footer/Footer.style.js
+++ src/components/global/footer/Footer.style.js
@@ -2,12 +2,55 @@ import { css } from '@emotion/react';
 
 export const styles = {
   footerContainer() {
-    return css``;
+    return css`
+      border-top: 1px solid rgba(25, 25, 25, 0.2);
+      padding: 45px 100px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+
+      display: flex;
+      justify-content: space-between;
+    `;
   },
   footerLeftPanel() {
-    return css``;
+    return css`
+      width: fit-content;
+
+      display: flex;
+      flex-direction: column;
+
+      h2 {
+        font-size: 24px;
+        font-weight: 900;
+        margin-bottom: 12px;
+      }
+
+      .teamInfo {
+        font-size: 14px;
+        margin-bottom: 8px;
+        color: #9a9a9a;
+      }
+
+      & > span:last-child {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 15px;
+        font-weight: 600;
+        margin-top: 2px;
+      }
+    `;
   },
   footerRightPanel() {
-    return css``;
+    return css`
+      display: flex;
+      gap: 60px;
+
+      div {
+        background-color: black;
+        width: 100px;
+        height: 100px;
+      }
+    `;
   },
 };
