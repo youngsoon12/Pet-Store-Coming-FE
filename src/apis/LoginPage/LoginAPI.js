b06a78a47b736c92ae93668205dc253eb67c6f28
JGW-Korea <jgw6372@gmail.com>
2024-10-26 19:35:11 +0900

    feat: Login API 설계 시작 (#3)

--- src/apis/LoginPage/LoginAPI.js
+++ src/apis/LoginPage/LoginAPI.js
@@ -4,6 +4,9 @@ export class LoginAPI {
   #REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
   #APP_REDIRECT_URL = import.meta.env.VITE_APP_REDIRECT_URL;
 
+  // 로그인 시도 API
+  async requestLogin(email, password) {}
+
   // 카카오 인가 코드 발급
   async fetchAccessToken(code) {
     try {
