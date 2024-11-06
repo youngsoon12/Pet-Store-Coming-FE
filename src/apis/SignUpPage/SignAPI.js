62122ca2ea4a544a8ff0326c98552f1d7fd0dc31
JGW-Korea <jgw6372@gmail.com>
2024-11-07 04:22:53 +0900

    feat: Register, Login 기능 및 페이지 구현 완료

---
+++ src/apis/SignUpPage/SignAPI.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export class SignUpAPI {
+  #BASE_URL = import.meta.env.VITE_API_URL;
+
+  async fetchSignUp(data, setErros) {
+    try {
+      await axios.post(`${this.#BASE_URL}/auth/sign-up`, data);
+      return true;
+    } catch (error) {
+      if (error.response) {
+        const err = error.response.data;
+
+        switch (err.errorCode) {
+          case 'DUPLICATE_EMAIL':
+            setErros((prev) => ({
+              ...prev,
+              email: '중복된 이메일이 존재합니다.',
+            }));
+        }
+      }
+
+      return false;
+    }
+  }
+}
--- src/pages/SignupSuccess/SignupSuccess.jsx
+++ src/pages/SignupSuccess/SignupSuccess.jsx
@@ -1,44 +1,72 @@
 /** @jsxImportSource @emotion/react */
 import { styles } from './SignupSuccess.style';
 import Button from '../../components/global/Button/Button';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function SignupSuccess() {
-  const name = '원정투수';
+  const location = useLocation();
   const navigate = useNavigate();
+
+  const { state } = location;
+
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    setUserInfo(state);
+  }, [state]);
+
   return (
     <>
-      <div css={styles.container}>
-        <div css={styles.main}>
-          <span>{name}</span>님 반가워요
+      {userInfo && (
+        <div css={styles.container}>
+          <div css={styles.main}>
+            <span>{userInfo.name}</span>님 반가워요
+          </div>
+          <div css={styles.main}>
+            <span>꼬밍</span> 회원가입이 완료되었어요!
+          </div>
+          <div css={styles.sub}>
+            {userInfo.role === 'user'
+              ? '우리 아이를 등록하고 맞춤 상품을 추천받아보세요'
+              : '지금 스토어를 등록하여 꼬밍 회원들에게 맞춤 상품을 소개하세요.'}
+          </div>
+          <div css={styles.buttons}>
+            {userInfo.role === 'user' ? (
+              <>
+                <Button
+                  text={'우리 아이 등록 >'}
+                  fontSize={'14'}
+                  fontWeight={'bold'}
+                  width={'130'}
+                  onClick={() => {
+                    navigate('/petprofile');
+                  }}
+                />
+                <Button
+                  text={'꼬밍 홈으로 >'}
+                  fontSize={'14'}
+                  fontWeight={'bold'}
+                  width={'130'}
+                  onClick={() => {
+                    navigate('/');
+                  }}
+                />
+              </>
+            ) : (
+              <Button
+                text={'스토어 등록 >'}
+                fontSize={'14'}
+                fontWeight={'bold'}
+                width={'130'}
+                onClick={() => {
+                  navigate('/petprofile');
+                }}
+              />
+            )}
+          </div>
         </div>
-        <div css={styles.main}>
-          <span>꼬밍</span> 회원가입이 완료되었어요!
-        </div>
-        <div css={styles.sub}>
-          우리아이를 등록하고 맞춤 상품을 추천받아보세요
-        </div>
-        <div css={styles.buttons}>
-          <Button
-            text={'우리아이 등록 >'}
-            fontSize={'14'}
-            fontWeight={'bold'}
-            width={'130'}
-            onClick={() => {
-              navigate('/petprofile');
-            }}
-          />
-          <Button
-            text={'꼬밍 홈으로 >'}
-            fontSize={'14'}
-            fontWeight={'bold'}
-            width={'130'}
-            onClick={() => {
-              navigate('/');
-            }}
-          />
-        </div>
-      </div>
+      )}
     </>
   );
 }
